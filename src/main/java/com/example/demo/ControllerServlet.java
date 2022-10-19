00514d153f8ae56b2667d6adc10430d5c3f5671c
akorton <akortysh@yandex.ru>
2022-10-19 18:09:37 +0300

    It is working. I dont know why

--- src/main/java/com/example/demo/ControllerServlet.java
+++ src/main/java/com/example/demo/ControllerServlet.java
@@ -8,6 +8,6 @@ import jakarta.servlet.http.*;
 public class ControllerServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>First lab</title>
+    <title>Second lab</title>
     <style>
 
         body{
